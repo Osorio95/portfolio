9e6bdd37d08a4e396109c14fef8065dc70fb6958
David <david.lml.95@gmail.com>
2023-11-04 18:42:01 -0400

    more bug fixes, ready to deploy

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Text from "@/components/ui/text"
-import Image from "next/image"
 import Slider from 'react-slick'
 
-import { Popover, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
